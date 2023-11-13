e5cc6ba654fb145ef4ee6546134a671880dc43b5
MertalBahar <mertalbahar@windowslive.com>
2023-11-13 20:51:08 +0300

    Product app created. 1. Product app created, urls and views adjusted. 2. Settings and main urls adjusted.

--- btkShop/urls.py
+++ btkShop/urls.py
@@ -6,5 +6,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('', include('home.urls')),
+    path('product/', include('product.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
