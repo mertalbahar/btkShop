4c3886953b0f31e2eb06d5b4e89cace41786d86e
MertalBahar <mertalbahar@windowslive.com>
2023-11-14 13:28:39 +0300

    Product app templates created. 1. Product index and detail created with template. 2. Product cart and checkout created with template. 3. Views and urls adjusted. 4. Navbar links updated.

--- product/views.py
+++ product/views.py
@@ -2,4 +2,28 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'product/index.html')
+    context = {
+        'page': 'product'
+    }
+    return render(request, 'product/index.html', context)
+
+
+def detail(request):
+    context = {
+        'page': 'detail'
+    }
+    return render(request, 'product/detail.html', context)
+
+
+def cart(request):
+    context = {
+        'page': 'cart'
+    }
+    return render(request, 'product/cart.html', context)
+
+
+def checkout(request):
+    context = {
+        'page': 'checkout'
+    }
+    return render(request, 'product/checkout.html', context)
