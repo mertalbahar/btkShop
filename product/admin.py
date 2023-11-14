18d7b94e1f897273828de6dd74e5715244e4679c
MertalBahar <mertalbahar@windowslive.com>
2023-11-14 14:29:21 +0300

    Product models created. Admin panel adjusted. 1. Category model created with parent relations. 2. Product model created and many to one relations connected between category and product with foreignkey. 3. Categories and products created in admin panel for samples.

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status')
+    list_editable = ('status',)
+    readonly_fields = ('created_at', 'slug',)
+    list_filter = ('status',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category_tag', 'slug', 'status')
+    list_editable = ('status',)
+    readonly_fields = ('created_at', 'slug')
+    list_filter = ('status',)
