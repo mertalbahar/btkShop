76e9d2f57f81b000f1e828b61e35bbaee33015c7
MertalBahar <mertalbahar@windowslive.com>
2023-11-14 10:21:01 +0300

    Home app templates created. 1. Home index created with template. 2. Contact, about, reference created with template. 3. Category menu closed and slider removed except home page. 4. Navbar links activated.

--- home/views.py
+++ home/views.py
@@ -2,4 +2,28 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    context = {
+        'page': 'home'
+    }
+    return render(request, 'home/index.html', context)
+
+
+def contact(request):
+    context = {
+        'page': 'contact'
+    }
+    return render(request, 'home/contact.html', context)
+
+
+def about(request):
+    context = {
+        'page': 'about'
+    }
+    return render(request, 'home/about.html', context)
+
+
+def reference(request):
+    context = {
+        'page': 'reference'
+    }
+    return render(request, 'home/reference.html', context)
